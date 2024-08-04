d9050aedede049e35b762024fb89247548626244
Natasha Png <natashapng.png@gmail.com>
2024-08-04 13:25:33 +1000

    idk

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './components/Todo'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>My To Do List</h1>
+      <div>
+        <h2>Finish FES</h2>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
