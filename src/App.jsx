4bee24549984185a9e7553055dfcea35f9a4e4b8
Natasha Png <natashapng.png@gmail.com>
2024-08-14 23:28:52 +1000

    user details page (not users list)

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ import Users from "./pages/Users"
 function App() {
   return (
       <Router>
-        <Nav />
+        {/* <Nav /> */}
         <Routes>
           <Route path="/" element={<Home/>}/>
-          <Route path="/users/:username" element={<Users/>}/>
+          <Route path="/users/:id" element={<Users/>}/>
         </Routes>
       </Router>
   
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,13 +1,29 @@
 import { useParams } from "react-router-dom"
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from "react-router-dom";
 
 function Users() {
-    const { username } = useParams();
+    const { id } = useParams();
+    const [user, setUser] = useState({})
+    // note useState({}), its a {} inside because user is singular, hence use objevt instead of array
 
-    // useEffect(() => {
-    //     // fetch(`https://api.instagram.com/${usermame}`)
-    // },[])
-    console.log(username)
-    return <h1>{username}</h1>
+    async function fetchUser() {
+        const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+        setUser(data)
+    }
+    useEffect(() => {
+        fetchUser()
+    }, [])
+    return (
+        <div>
+            <Link to = '/'>Go back</Link>
+
+      <p>{user.id}</p> 
+    <p>{user.name}</p>
+    <p>{user.email}</p>
+    <p>{user.username}</p>
+        </div>)
 }
 
 export default Users
