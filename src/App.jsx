eefb4c7a233674ded6a7e248f49f226278713616
Natasha Png <natashapng.png@gmail.com>
2024-08-14 16:24:15 +1000

    Making nav a component in a seperate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Nav from "./components/Nav";
 
 
 function App() {
   return (
       <Router>
-        {/* Everything has to be wrapped around Router */}
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
-          
-          
-          {
-          /*
-          
-          This makes the page refresh even if you click on the same route
-
-          <a href="/">Home</a>
-          <a href="/about">About</a>
-          <a href="/contact">Contact</a> 
-          
-          */}
-        </nav>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}></Route>
